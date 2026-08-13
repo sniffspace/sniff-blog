fd6741143f7a40fb1c375b575cd889b82f39940e
amit <amit.datatech@gmail.com>
2026-08-13 13:35:56 +0530

    app store and play store icon updated

--- wp-content/themes/sniffspace/footer.php
+++ wp-content/themes/sniffspace/footer.php
@@ -25,21 +25,14 @@ $sniffspace_asset_url  = get_template_directory_uri() . '/assets/images/';
 					<p><?php esc_html_e( 'Download the Sniffspace App', 'sniffspace' ); ?></p>
 					<div class="ss-store-row">
 						<a class="ss-store-button ss-store-google" href="https://play.google.com/store/search?q=sniffspace&amp;c=apps&amp;hl=en-IN" aria-label="<?php esc_attr_e( 'Get it on Google Play', 'sniffspace' ); ?>" title="<?php esc_attr_e( 'Get it on Google Play', 'sniffspace' ); ?>">
-							<svg aria-hidden="true" viewBox="0 0 24 24" focusable="false">
-								<path fill="#34a853" d="M3.6 2.8c-.2.2-.3.6-.3 1v16.4c0 .4.1.7.3 1l8.9-9.2-8.9-9.2Z"/>
-								<path fill="#4285f4" d="m14.9 9.5-2.4 2.5 2.4 2.5 3.2-1.9c1.1-.6 1.1-1.6 0-2.2l-3.2-1.9Z"/>
-								<path fill="#fbbc04" d="m3.6 2.8 8.9 9.2 2.4-2.5L5.9 4.3c-.8-.5-1.6-1-2.3-1.5Z"/>
-								<path fill="#ea4335" d="m3.6 21.2 2.3-1.5 9-5.2-2.4-2.5-8.9 9.2Z"/>
-							</svg>
+							<svg fill="#000000" width="32px" height="32px" viewBox="-1 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m3.751.61 13.124 7.546-2.813 2.813zm-2.719-.61 12.047 12-12.046 12c-.613-.271-1.033-.874-1.033-1.575 0-.023 0-.046.001-.068v.003-20.719c-.001-.019-.001-.042-.001-.065 0-.701.42-1.304 1.022-1.571l.011-.004zm19.922 10.594c.414.307.679.795.679 1.344 0 .022 0 .043-.001.065v-.003c.004.043.007.094.007.145 0 .516-.25.974-.636 1.258l-.004.003-2.813 1.593-3.046-2.999 3.047-3.047zm-17.203 12.796 10.312-10.359 2.813 2.813z"/></svg>
 							<span>
 								<small><?php esc_html_e( 'GET IT ON', 'sniffspace' ); ?></small>
 								<strong><?php esc_html_e( 'Google Play', 'sniffspace' ); ?></strong>
 							</span>
 						</a>
 						<a class="ss-store-button ss-store-apple" href="https://apps.apple.com/app/sniffspace/id6467835662" aria-label="<?php esc_attr_e( 'Download on the App Store', 'sniffspace' ); ?>" title="<?php esc_attr_e( 'Download on the App Store', 'sniffspace' ); ?>">
-							<svg aria-hidden="true" viewBox="0 0 24 24" focusable="false">
-								<path fill="currentColor" d="M16.8 12.6c0-2 1.6-3 1.7-3.1-.9-1.4-2.4-1.6-2.9-1.6-1.2-.1-2.4.7-3 0-.6-.6-1.6-.7-2.6-.7-1.3 0-2.5.8-3.2 1.9-1.4 2.4-.4 6 1 8 .7 1 1.5 2.1 2.6 2.1 1 0 1.4-.7 2.7-.7 1.2 0 1.6.7 2.7.7s1.8-1 2.5-2c.8-1.1 1.1-2.3 1.1-2.4 0 0-2.6-1-2.6-4.2ZM14.8 6.6c.6-.7 1-1.7.9-2.6-.9 0-1.9.6-2.5 1.3-.6.6-1 1.6-.9 2.5 1 0 1.9-.5 2.5-1.2Z"/>
-							</svg>
+							<svg fill="#000000" width="32px" height="32px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.564 13.862c-.413.916-.612 1.325-1.144 2.135-.742 1.13-1.79 2.538-3.087 2.55-1.152.01-1.448-.75-3.013-.741-1.564.008-1.89.755-3.043.744-1.297-.012-2.29-1.283-3.033-2.414-2.077-3.16-2.294-6.87-1.013-8.843.91-1.401 2.347-2.221 3.697-2.221 1.375 0 2.24.754 3.376.754 1.103 0 1.775-.756 3.365-.756 1.2 0 2.474.655 3.381 1.785-2.972 1.629-2.49 5.873.514 7.007zM12.463 3.808c.577-.742 1.016-1.788.857-2.858-.944.065-2.047.665-2.692 1.448-.584.71-1.067 1.763-.88 2.787 1.03.031 2.096-.584 2.715-1.377z"/></svg>
 							<span>
 								<small><?php esc_html_e( 'Download on the', 'sniffspace' ); ?></small>
 								<strong><?php esc_html_e( 'App Store', 'sniffspace' ); ?></strong>
